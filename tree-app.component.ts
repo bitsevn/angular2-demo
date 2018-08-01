fb849183a1deaf25e304d485ec671896feb3bc59
mumbaisurfer <mumbaisurfer@users.noreply.github.com>
2018-08-01 10:32:08 +0530

    Create tree-app.component.ts

---
+++ tree-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'tree-app',
+    templateUrl: './tree-app.component.html',
+    styleUrls: ['./tree-app.component.scss']
+})
+export class TreeAppComponent {
+
+}
